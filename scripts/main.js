c4509cf5574f4f94f699a114a09b992b6e8bb857
Viktoriia-Piskova <viktoriapiskova@gmail.com>
2022-10-28 18:40:14 +0300

    Car card display

--- scripts/main.js
+++ scripts/main.js
@@ -2,4 +2,11 @@
 import {carsData} from './carsData.js'
 
 
+
+//liked item
+let icon = document.querySelector('.like-btn');
+icon.onclick = function(){
+  icon.classList.toggle('active');
+}
+
 console.log(carsData);
